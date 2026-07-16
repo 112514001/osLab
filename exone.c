4b6a14ca6dc06d8941c2e0280a0b30a8ffa487af
112514001 <112514001@kanchiuniv.ac.in>
2026-07-16 07:46:42 +0000

    First sample pogram

---
+++ exone.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("Current Directory\n");
+    system("pwd");
+
+    printf("\nFiles\n");
+    system("ls -l");
+
+    printf("\nCreating Directory\n");
+    system("mkdir Demo");
+
+    printf("\nListing Files Again\n");
+    system("rmdir Demo");
+
+    return 0;
+}
